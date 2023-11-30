946ae01f1d589cfa439d38b68f692a6c04a8a64c
Ex-007 <agbebibidemi02@gmail.com>
2023-11-30 23:54:26 +0100

    added new profile details and stylings

---
+++ newprofile.js
@@ -0,0 +1,37 @@
+
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
+
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBmY2XtNwXZrHeE5za2cp7sOFYOKjSvdCQ",
+    authDomain: "school-newusersign.firebaseapp.com",
+    projectId: "school-newusersign",
+    storageBucket: "school-newusersign.appspot.com",
+    messagingSenderId: "382800829354",
+    appId: "1:382800829354:web:145aeb3f8e346c016129d3"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  import {getAuth, signOut} from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
+
+  let auth = getAuth()
+  let signOutBtn = document.getElementById('signOut')
+
+  function signOutUser(){
+    signOut(auth)
+    .then(response => {
+        alert("You've signed out")
+        window.location.href = 'account.html'
+        console.log(response)
+    })
+    .catch(error => {
+        console.error(error);
+    })
+  }
+
+  signOutBtn.addEventListener('click', signOutUser)
